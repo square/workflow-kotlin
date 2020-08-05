f95f0d16e7a4c6408a61d857d3827af347d0ee4d
Zach Klippenstein <zach.klippenstein@gmail.com>
2020-08-05 10:43:57 -0700

    Update AGP to alpha07.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -27,7 +27,7 @@ object Versions {
 
 @Suppress("unused")
 object Dependencies {
-  const val android_gradle_plugin = "com.android.tools.build:gradle:4.2.0-alpha05"
+  const val android_gradle_plugin = "com.android.tools.build:gradle:4.2.0-alpha07"
 
   object AndroidX {
     const val appcompat = "androidx.appcompat:appcompat:1.1.0"
