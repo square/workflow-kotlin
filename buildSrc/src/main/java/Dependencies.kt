319d921bb7c8635dae8e98dfe8d4a3be0297d7c5
Zach Klippenstein <zach.klippenstein@gmail.com>
2020-09-14 08:25:34 -0700

    Upgrade AGP to 4.2.0-alpha10.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -27,7 +27,7 @@ object Versions {
 
 @Suppress("unused")
 object Dependencies {
-  const val android_gradle_plugin = "com.android.tools.build:gradle:4.2.0-alpha07"
+  const val android_gradle_plugin = "com.android.tools.build:gradle:4.2.0-alpha10"
 
   object AndroidX {
     const val appcompat = "androidx.appcompat:appcompat:1.1.0"
