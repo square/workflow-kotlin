5dc7af1af902ed245f0200e8c803f1841f7862ef
Ray Ryan <ray@squareup.com>
2020-10-27 11:54:54 -0700

    Makes snapshots lazy again.
    `renderWorkflowIn` uses a `MutableStateFlow` as the source of its
    `RenderingAndSnapshot` stream, and that class does an equality check as an
    optimization.
    
    Fix is to stop making `RenderingAndSnapshot` a data class, just use the
    default identity equality.
    
    closes #223

---
+++ workflow-runtime/src/test/java/com/squareup/workflow1/RenderingAndSnapshotTest.kt
@@ -0,0 +1,24 @@
+package com.squareup.workflow1
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertSame
+
+class RenderingAndSnapshotTest {
+  @Test fun destructuring() {
+    val snapshot = TreeSnapshot(Snapshot.of(0)) { emptyMap() }
+    val (r, t) = RenderingAndSnapshot("Rendering", snapshot)
+    assertEquals(r, "Rendering")
+    assertSame(t, snapshot)
+  }
+
+  @Test fun `identity equality`() {
+    val snapshot = TreeSnapshot(Snapshot.of(0)) { emptyMap() }
+    val me = RenderingAndSnapshot("Rendering", snapshot)
+    val you = RenderingAndSnapshot("Rendering", snapshot)
+
+    assertEquals(me, me)
+    assertNotEquals(me, you)
+  }
+}
