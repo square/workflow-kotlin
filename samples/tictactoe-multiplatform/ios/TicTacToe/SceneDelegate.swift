68a0ffc806f4e3c379f108447b1b46077f2b2442
Brady Aiello <brady@touchlab.co>
2021-08-16 10:10:23 -0700

    iOS app runs normally (just w/ workflow-swift)

--- samples/tictactoe-multiplatform/ios/TicTacToe/SceneDelegate.swift
+++ samples/tictactoe-multiplatform/ios/TicTacToe/SceneDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftUI
+import WorkflowUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -19,12 +20,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        //let contentView = ContentView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = ContainerViewController(workflow: MainWorkflow())
             self.window = window
             window.makeKeyAndVisible()
         }
