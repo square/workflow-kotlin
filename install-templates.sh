8e85cecbf204a280b3fa14bf156cf547b6e4f907
saied89 <saiedmomen@gmail.com>
2021-09-06 03:37:07 +0430

    add linux file template instalation

--- install-templates.sh
+++ install-templates.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Installs Workflow file templates for IntelliJ and Android Studio.
 
-echo "Installing Workflow file templates..."
 
+OS="$(uname -s)"
+echo "Installing Workflow file templates on $OS system..."
+ideaConfigPath=""
+if [[ "$OS" == Linux ]]; then
+  ideaConfigPath='.config'
+elif [[ "$OS" == Darwin ]]; then
+  ideaConfigPath="Library/Application\ Support"
+fi
 TEMPLATES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/fileTemplates"
 
-for i in $HOME/Library/Application\ Support/Google/AndroidStudio* \
-         $HOME/Library/Application\ Support/JetBrains/IdeaIC*
+for i in $HOME/$ideaConfigPath/Google/AndroidStudio* \
+         $HOME/$ideaConfigPath/JetBrains/IdeaIC*
 do
   if [[ -d "$i" ]]; then
     mkdir -p "$i/fileTemplates"
