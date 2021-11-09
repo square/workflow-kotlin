b4565c26749b1946aed163427ef6f6ff35def0cd
Ray Ryan <ray@squareup.com>
2021-11-09 11:31:17 -0800

    Disables HelloWorkflowFragmentAppTest on API 21 b/c LeakCanary
    Or more likely, b/c LeakCanary exposes an obscure bug in Jetpack
    and life is just too short.
    
    Fixes #582.

--- samples/hello-workflow-fragment/src/androidTest/java/com/squareup/sample/helloworkflowfragment/HelloWorkflowFragmentAppTest.kt
+++ samples/hello-workflow-fragment/src/androidTest/java/com/squareup/sample/helloworkflowfragment/HelloWorkflowFragmentAppTest.kt
@@ -1,11 +1,13 @@
 package com.squareup.sample.helloworkflowfragment
 
+import android.os.Build
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
 import com.squareup.workflow1.ui.WorkflowUiExperimentalApi
 import com.squareup.workflow1.ui.internal.test.inAnyView
 import org.hamcrest.Matchers.containsString
@@ -13,6 +15,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+// Life is too short to debug why LeakCanary breaks this on API 21
+// https://github.com/square/workflow-kotlin/issues/582
+@SdkSuppress(minSdkVersion = Build.VERSION_CODES.M)
 @RunWith(AndroidJUnit4::class)
 @OptIn(WorkflowUiExperimentalApi::class)
 class HelloWorkflowFragmentAppTest {
