504f94d4fcd89b22e4c1b2fce1cf7a89ea01f11a
Rick Busarow <rickbusarow@gmail.com>
2022-01-24 08:24:30 -0600

    WIP switching to laptop

--- internal-testing-utils/build.gradle.kts
+++ internal-testing-utils/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
   kotlin("jvm")
-
-  id("com.rickbusarow.gradle-dependency-sync") version "0.11.4"
 }
 
 apply(from = rootProject.file(".buildscript/configure-maven-publish.gradle"))
@@ -17,9 +15,4 @@ dependencies {
   testImplementation(libs.kotlin.test.jdk)
   testImplementation(libs.junit)
   testImplementation(libs.truth)
-
-
-
-  dependencySync("com.android.tools.build:gradle:7.0.0")
-
 }
