2bc8690495823dd6c0e22f534034c96efaee8299
Ray Ryan <ray@squareup.com>
2022-04-14 12:44:05 -0700

    Fixes unused imports on LoaderSpinner

--- benchmarks/performance-poetry/complex-poetry/src/main/java/com/squareup/benchmarks/performance/complex/poetry/views/LoaderSpinner.kt
+++ benchmarks/performance-poetry/complex-poetry/src/main/java/com/squareup/benchmarks/performance/complex/poetry/views/LoaderSpinner.kt
@@ -6,13 +6,9 @@ import android.view.ViewGroup.LayoutParams
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ProgressBar
 import com.squareup.workflow1.ui.AndroidOverlay
-import com.squareup.workflow1.ui.AndroidScreen
-import com.squareup.workflow1.ui.ScreenViewFactory
-import com.squareup.workflow1.ui.ScreenViewHolder
 import com.squareup.workflow1.ui.ViewEnvironment
 import com.squareup.workflow1.ui.WorkflowUiExperimentalApi
 import com.squareup.workflow1.ui.container.OverlayDialogFactory
-import kotlin.reflect.KClass
 
 @OptIn(WorkflowUiExperimentalApi::class)
 object LoaderSpinner : AndroidOverlay<LoaderSpinner> {
