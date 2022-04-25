f4e2f89f39bb035527b5011b424f1a93e28e784e
Vinay B. Nagaraj <bnvinay92@gmail.com>
2022-04-25 12:57:34 +0400

    Prefer project repositories instead of failing
    Native source sets implicitly add the ivy repo:
    https://github.com/JetBrains/kotlin/blob/dc210fa6efee266983506c6b57bf9c9b6867d897/libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/NativeCompilerDownloader.kt#L101

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ gradleEnterprise {
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
   repositories {
     mavenCentral()
     google()
