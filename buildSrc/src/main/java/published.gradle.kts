12e769e2cac480f9d7c50aa4d065996f68f57307
Rick Busarow <rickbusarow@gmail.com>
2022-06-23 12:47:53 -0500

    add `checkVersionIsSnapshot` task to CI and `publish-snapshot` actions

--- buildSrc/src/main/java/published.gradle.kts
+++ buildSrc/src/main/java/published.gradle.kts
@@ -16,3 +16,13 @@ version = project.property("VERSION_NAME") as String
 mavenPublish {
   sonatypeHost = SonatypeHost.S01
 }
+
+tasks.register("checkVersionIsSnapshot") {
+  doLast {
+    val expected = "-SNAPSHOT"
+    require((version as String).endsWith(expected)) {
+      "The project's version name must be suffixed with `$expected` when checked in" +
+        " to the main branch, but instead it's `$version`."
+    }
+  }
+}
