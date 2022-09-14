2438f100ef87cedfda604cc63e753a0f4dc95452
Ray Ryan <ray@squareup.com>
2022-09-14 10:21:07 -0700

    `WorkflowViewStubLifecycleTest` is green.
    …and is exercising both `VisualFactory` implementations and a `VisualFactory` based wrapper of `ScreenViewFactoryFinder` for backward compatibility.
    
    Still a lot of TODOs in `AndroidViewFactoryKey` in particular, but that's a massive milestone. Gives me a lot of confidence in this approach.
    
    Biggest bit of remaining uncertainty is all the nooks and crannies of `OverlayDialogHolder`, in support of
    
    - back button hooks
    - bounds management
    - modal behavior
    
    Also still need to demonstrate Compose integration, but the prototype @helios175 built gives me a lot of confidence there.

--- workflow-ui/core-android/src/main/java/com/squareup/workflow1/visual/AndroidViewMultiRendering.kt
+++ workflow-ui/core-android/src/main/java/com/squareup/workflow1/visual/AndroidViewMultiRendering.kt
@@ -7,6 +7,23 @@ import com.squareup.workflow1.ui.NamedScreen
 import com.squareup.workflow1.ui.Screen
 import com.squareup.workflow1.ui.WorkflowUiExperimentalApi
 
+/**
+ * General support for classic Android container [View]s, used by `WorkflowViewStub`
+ * et al. To be registered as a [VisualEnvironment] service keyed to [AndroidViewMultiRendering].
+ *
+ * Expected to be able to handle renderings of all type. That is, it:
+ *
+ * - Delegates to [AndroidViewFactoryKey] to handle renderings that are mapped directly
+ *   to [View] based factories
+ * - Supports general wrapper types like [Named], which are not [View] specific
+ * - Handles conversions between types, e.g. wrapping any `Compose` based [VisualFactory]
+ *   with `ComposeView`.
+ *
+ * The default implementation does not handle that last style of conversion, to avoid
+ * making the general `core-android` module depend on the Compose runtime. The `compose`
+ * module will bind an alternative implementation that adds these converters, for apps
+ * to bind to the [AndroidViewMultiRendering] key.
+ */
 @OptIn(WorkflowUiExperimentalApi::class)
 public class AndroidViewMultiRendering : MultiRendering<Context, View>() {
 
@@ -30,16 +47,15 @@ public class AndroidViewMultiRendering : MultiRendering<Context, View>() {
    * Compose integration will provide an alternative implementation that
    * extends this one to wrap Compose-specific VisualFactories in ComposeView.
    */
-  public companion object : VisualEnvironmentKey<AndroidViewFactory>() {
-    override val default: AndroidViewFactory
-      get() = object : AndroidViewFactory {
+  public companion object : VisualEnvironmentKey<AndroidViewFactory<Any>>() {
+    override val default: AndroidViewFactory<Any>
+      get() = object : AndroidViewFactory<Any> {
         override fun createOrNull(
           rendering: Any,
           context: Context,
           environment: VisualEnvironment
         ): VisualHolder<Any, View>? {
-          val concreteBindings = environment[AndroidViewFactoryKey]
-          concreteBindings.createOrNull(rendering, context, environment)?.let {
+          environment[AndroidViewFactoryKey].createOrNull(rendering, context, environment)?.let {
             return it
           }
 
--- workflow-ui/core-common/src/main/java/com/squareup/workflow1/visual/MultiRendering.kt
+++ workflow-ui/core-common/src/main/java/com/squareup/workflow1/visual/MultiRendering.kt
@@ -7,15 +7,36 @@ import com.squareup.workflow1.ui.compatible
  * Base support class for containers (stubs) that can show a series of renderings that
  * might be of different types or [incompatible][compatible], and so require different
  * factories and holders.
+ *
+ * Provides the hooks needed for lifecycle management, including
+ * view state concerns, so that creation of a [VisualHolder] is decoupled from the
+ * first call to its [VisualHolder.update] function. This is achieved via the
+ * `onNewVisual` lambda that callers must provide to [replaceWith]. That lambda
+ * is passed another, `doFirstUpdate`, which must be called immediately (enforced
+ * at runtime). The default value for `onNewVisual` does nothing else, and may be
+ * all that is needed for Compose. If not, we should drop the default and force
+ * callers to be thoughtful.
+ *
+ * Expect to wind up with one implementation of this for each [VisualT] value in
+ * a system:
+ *
+ * - Android View
+ * - Android Dialog
+ * - @Composable fun () -> Unit
+ *
+ * And maybe a fourth for mapping Overlay to Compose's Dialog()? Or perhaps the third
+ * flavor can cover that case too. Perhaps we'll decide that we always use classic
+ * APIs for that.
  */
 @WorkflowUiExperimentalApi
 public abstract class MultiRendering<UiContextT, VisualT> {
   private var rendering: Any? = null
   private var holder: VisualHolder<Any, VisualT>? = null
 
-  public val visual: VisualT get() = requireNotNull(holder?.visual) {
-    "visual of $this is null, probably replaceWith hasn't been called yet."
-  }
+  public val visual: VisualT
+    get() = requireNotNull(holder?.visual) {
+      "visual of $this is null, probably replaceWith hasn't been called yet."
+    }
 
   public val visualOrNull: VisualT? get() = holder?.visual
 
