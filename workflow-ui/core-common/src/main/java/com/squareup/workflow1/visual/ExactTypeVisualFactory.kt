d9da27ac623fba52482bcc6ee3413b812430c2f1
Ray Ryan <ray@squareup.com>
2022-11-07 09:26:22 -0800

    Better home for forItemsWhere

--- workflow-ui/core-common/src/main/java/com/squareup/workflow1/visual/ExactTypeVisualFactory.kt
+++ workflow-ui/core-common/src/main/java/com/squareup/workflow1/visual/ExactTypeVisualFactory.kt
@@ -25,31 +25,3 @@ public class ExactTypeVisualFactory<ContextT, VisualT>(
     rendering, context, environment
   )
 }
-
-@WorkflowUiExperimentalApi
-public fun <ContextT, RenderingT, VisualT> VisualFactory<ContextT, RenderingT, VisualT>.forItemsWhere(
-  predicate: (RenderingT) -> Boolean
-): VisualFactory<ContextT, RenderingT, VisualT> {
-  val delegate = this
-  return object : VisualFactory<ContextT, RenderingT, VisualT> {
-    override fun createOrNull(
-      rendering: RenderingT,
-      context: ContextT,
-      environment: VisualEnvironment
-    ): VisualHolder<RenderingT, VisualT>? {
-      return if (predicate(rendering)) {
-        delegate.createOrNull(
-          rendering,
-          context,
-          environment
-        )?.let { delegateHolder ->
-          object : VisualHolder<RenderingT, VisualT> by delegateHolder {
-            override fun update(rendering: RenderingT): Boolean {
-              return predicate(rendering) && delegateHolder.update(rendering)
-            }
-          }
-        }
-      } else null
-    }
-  }
-}
