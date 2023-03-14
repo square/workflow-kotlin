f35524991997f9815f7dbbb366be0d12397ad148
Ray Ryan <ray@squareup.com>
2023-03-14 16:35:37 -0700

    Eliminates espresso hacks
    Trying to reduce Espresso flakes by being more careful and conventional about `RootMatcher` use; about targetting back button presses; and by using `recreate()` for configuration changes.

--- samples/todo-android/app/src/androidTest/java/com/squareup/sample/mainactivity/TodoAppTest.kt
+++ samples/todo-android/app/src/androidTest/java/com/squareup/sample/mainactivity/TodoAppTest.kt
@@ -1,6 +1,8 @@
 package com.squareup.sample.mainactivity
 
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -11,10 +13,7 @@ import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
 import com.squareup.sample.todo.R
 import com.squareup.sample.todo.ToDoActivity
-import com.squareup.workflow1.ui.WorkflowUiExperimentalApi
 import com.squareup.workflow1.ui.internal.test.IdlingDispatcherRule
-import com.squareup.workflow1.ui.internal.test.actuallyPressBack
-import com.squareup.workflow1.ui.internal.test.inAnyView
 import leakcanary.DetectLeaksAfterTestSuccess
 import org.hamcrest.Matchers.allOf
 import org.junit.After
@@ -25,12 +24,11 @@ import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@OptIn(WorkflowUiExperimentalApi::class)
 class TodoAppTest {
 
   private val scenarioRule = ActivityScenarioRule(ToDoActivity::class.java)
 
-  @get:Rule val rules = RuleChain.outerRule(DetectLeaksAfterTestSuccess())
+  @get:Rule val rules: RuleChain = RuleChain.outerRule(DetectLeaksAfterTestSuccess())
     .around(scenarioRule)
     .around(IdlingDispatcherRule)
   private val uiDevice by lazy { UiDevice.getInstance(getInstrumentation()) }
@@ -49,13 +47,13 @@ class TodoAppTest {
     val isPortrait = uiDevice.displayWidth < uiDevice.displayHeight
     if (!isPortrait) uiDevice.setOrientationLeft()
 
-    inAnyView(withText("Daily Chores"))
+    onView(withText("Daily Chores"))
       .check(matches(allOf(isDisplayed())))
       .perform(click())
-    inAnyView(withId(R.id.item_container))
+    onView(withId(R.id.item_container))
       .check(matches(isDisplayed()))
-    actuallyPressBack()
-    inAnyView(withId(R.id.todo_lists_container))
+      .perform(pressBack())
+    onView(withId(R.id.todo_lists_container))
       .check(matches(isDisplayed()))
   }
 
@@ -63,12 +61,12 @@ class TodoAppTest {
     val isPortrait = uiDevice.displayWidth < uiDevice.displayHeight
     if (isPortrait) uiDevice.setOrientationLeft()
 
-    inAnyView(withText("Daily Chores"))
+    onView(withText("Daily Chores"))
       .check(matches(allOf(isDisplayed())))
       .perform(click())
-    inAnyView(withId(R.id.item_container))
+    onView(withId(R.id.item_container))
       .check(matches(isDisplayed()))
-    inAnyView(withId(R.id.todo_lists_container))
+    onView(withId(R.id.todo_lists_container))
       .check(matches(isDisplayed()))
   }
 }
