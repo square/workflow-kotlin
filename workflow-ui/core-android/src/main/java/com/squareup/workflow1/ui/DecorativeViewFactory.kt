4e36d439fa109e785ed9b4299328e815ff45189b
Ray Ryan <ray@squareup.com>
2023-03-21 17:34:55 -0700

    Fixes up a few stale names after wrapper update.

--- workflow-ui/core-android/src/main/java/com/squareup/workflow1/ui/DecorativeViewFactory.kt
+++ workflow-ui/core-android/src/main/java/com/squareup/workflow1/ui/DecorativeViewFactory.kt
@@ -109,7 +109,7 @@ import kotlin.reflect.KClass
  * uses [map] to extract the [InnerT] instance from [OuterT] and makes the function call.
  */
 @Suppress("DEPRECATION")
-@Deprecated("Use ScreenViewFactory.toUnwrappingViewFactory")
+@Deprecated("Use ScreenViewFactory.map")
 @WorkflowUiExperimentalApi
 public class DecorativeViewFactory<OuterT : Any, InnerT : Any>(
   override val type: KClass<OuterT>,
