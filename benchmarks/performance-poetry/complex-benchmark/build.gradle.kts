0218acab60016185f8893d7e696991fa4aebf623
Stephen Edwards <sedwards@squareup.com>
2023-10-20 15:57:33 -0400

    Update Java to Java 17

--- benchmarks/performance-poetry/complex-benchmark/build.gradle.kts
+++ benchmarks/performance-poetry/complex-benchmark/build.gradle.kts
@@ -13,12 +13,12 @@ android {
   compileSdk = libsCatalog.version("compileSdk").toInt()
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
 
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
     freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
   }
 
--- build-logic/src/main/java/com/squareup/workflow1/buildsrc/AndroidDefaultsPlugin.kt
+++ build-logic/src/main/java/com/squareup/workflow1/buildsrc/AndroidDefaultsPlugin.kt
@@ -3,7 +3,7 @@ package com.squareup.workflow1.buildsrc
 import com.android.build.gradle.TestedExtension
 import com.squareup.workflow1.buildsrc.internal.libsCatalog
 import com.squareup.workflow1.buildsrc.internal.version
-import org.gradle.api.JavaVersion.VERSION_1_8
+import org.gradle.api.JavaVersion.VERSION_17
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -17,8 +17,8 @@ class AndroidDefaultsPlugin : Plugin<Project> {
 
       testedExtension.compileOptions { compileOptions ->
 
-        compileOptions.sourceCompatibility = VERSION_1_8
-        compileOptions.targetCompatibility = VERSION_1_8
+        compileOptions.sourceCompatibility = VERSION_17
+        compileOptions.targetCompatibility = VERSION_17
       }
 
       testedExtension.defaultConfig { defaultConfig ->
