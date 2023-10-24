30f0cd294bfdc6da8dcd0c11a9e90689b9a79768
Stephen Edwards <sedwards@squareup.com>
2023-10-24 10:36:19 -0400

    Let Kotlin determine correct simulator

--- build-logic/src/main/java/com/squareup/workflow1/buildsrc/KotlinMultiplatformExtensions.kt
+++ build-logic/src/main/java/com/squareup/workflow1/buildsrc/KotlinMultiplatformExtensions.kt
@@ -8,13 +8,6 @@ fun KotlinMultiplatformExtension.iosWithSimulatorArm64(target: Project) {
   ios()
   iosSimulatorArm64()
 
-  targets.withType(KotlinNativeTargetWithSimulatorTests::class.java) {
-    it.testRuns.configureEach {
-      // Kotlin 1.7.2x is hard-coded to "iPhone 13", which is no longer included by default in Xcode
-      it.deviceId = target.properties["workflow.iosSimulatorName"] as? String ?: "iPhone 14"
-    }
-  }
-
   sourceSets.getByName("iosSimulatorArm64Main") {
     it.dependsOn(sourceSets.getByName("iosMain"))
   }
