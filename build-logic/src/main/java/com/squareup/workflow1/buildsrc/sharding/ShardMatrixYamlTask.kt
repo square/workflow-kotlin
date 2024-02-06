273263d3099d6f7479c8d28c3c8babc097d10cb8
Rick Busarow <rickbusarow@gmail.com>
2024-02-05 18:05:09 -0600

    remove spaces before colons in Yaml
    this fixes Renovate's parsing of GHA workflows
    https://github.com/renovatebot/renovate/discussions/27088

--- build-logic/src/main/java/com/squareup/workflow1/buildsrc/sharding/ShardMatrixYamlTask.kt
+++ build-logic/src/main/java/com/squareup/workflow1/buildsrc/sharding/ShardMatrixYamlTask.kt
@@ -31,195 +31,201 @@ import kotlin.LazyThreadSafetyMode.NONE
  *   matrix configurations.
  * @property updateTaskName The name of the task that updates the test shard matrix.
  */
-abstract class ShardMatrixYamlTask @Inject constructor(
-  objectFactory: ObjectFactory
-) : DefaultTask() {
+abstract class ShardMatrixYamlTask
+  @Inject
+  constructor(
+    objectFactory: ObjectFactory
+  ) : DefaultTask() {
+    /** kotlin.yml */
+    @get:InputFile
+    @get:PathSensitive(RELATIVE)
+    val yamlFile = objectFactory.fileProperty()
 
-  /** kotlin.yml */
-  @get:InputFile
-  @get:PathSensitive(RELATIVE)
-  val yamlFile = objectFactory.fileProperty()
+    /**
+     * Used to identify the start of the matrix.
+     * Everything after this tag and before the end tag will be overwritten.
+     *
+     * ex: `### <start-my-shard-matrix>`
+     */
+    @get:Input abstract val startTagProperty: Property<String>
+    private val startTag: String
+      get() = startTagProperty.get()
 
-  /**
-   * Used to identify the start of the matrix.
-   * Everything after this tag and before the end tag will be overwritten.
-   *
-   * ex: `### <start-my-shard-matrix>`
-   */
-  @get:Input abstract val startTagProperty: Property<String>
-  private val startTag: String
-    get() = startTagProperty.get()
+    /**
+     * Used to identify the end of the matrix.
+     * ex: `### <end-my-shard-matrix>`
+     * */
+    @get:Input abstract val endTagProperty: Property<String>
+    private val endTag: String
+      get() = endTagProperty.get()
 
-  /**
-   * Used to identify the end of the matrix.
-   * ex: `### <end-my-shard-matrix>`
-   * */
-  @get:Input abstract val endTagProperty: Property<String>
-  private val endTag: String
-    get() = endTagProperty.get()
+    /** for `3`, the matrix value would be `[ 1, 2, 3]` */
+    @get:Input
+    abstract val numShards: Property<Int>
 
-  /** for `3`, the matrix value would be `[ 1, 2, 3]` */
-  @get:Input
-  abstract val numShards: Property<Int>
+    /**
+     * If true the file will be updated. If false, the task will fail if the matrix is out of date.
+     */
+    @get:Input
+    abstract val autoCorrect: Property<Boolean>
 
-  /**
-   * If true the file will be updated. If false, the task will fail if the matrix is out of date.
-   */
-  @get:Input
-  abstract val autoCorrect: Property<Boolean>
+    @get:Input
+    abstract val updateTaskName: Property<String>
 
-  @get:Input
-  abstract val updateTaskName: Property<String>
+    private val matrixSectionRegex by lazy(NONE) {
 
-  private val matrixSectionRegex by lazy(NONE) {
+      val startTagEscaped = Regex.escape(startTag)
+      val endTagEscaped = Regex.escape(endTag)
 
-    val startTagEscaped = Regex.escape(startTag)
-    val endTagEscaped = Regex.escape(endTag)
+      Regex("""( *)(.*$startTagEscaped.*\n)[\s\S]+?(.*$endTagEscaped)""")
+    }
 
-    Regex("""( *)(.*$startTagEscaped.*\n)[\s\S]+?(.*$endTagEscaped)""")
-  }
+    @TaskAction
+    fun execute() {
+      val ciFile = requireCiFile()
 
-  @TaskAction
-  fun execute() {
-    val ciFile = requireCiFile()
+      val ciText = ciFile.readText()
 
-    val ciText = ciFile.readText()
+      val newText = replaceYamlSections(ciText)
 
-    val newText = replaceYamlSections(ciText)
+      if (ciText != newText) {
 
-    if (ciText != newText) {
+        if (autoCorrect.get()) {
 
-      if (autoCorrect.get()) {
+          ciFile.writeText(newText)
 
-        ciFile.writeText(newText)
+          val message =
+            "Updated the test shard matrix in the CI file.\n" +
+              "\tfile://${yamlFile.get()}"
 
-        val message = "Updated the test shard matrix in the CI file.\n" +
-          "\tfile://${yamlFile.get()}"
+          services
+            .get(StyledTextOutputFactory::class.java)
+            .create("workflow-yaml-matrix")
+            .withStyle(StyledTextOutput.Style.Description)
+            .println(message)
 
-        services
-          .get(StyledTextOutputFactory::class.java)
-          .create("workflow-yaml-matrix")
-          .withStyle(StyledTextOutput.Style.Description)
-          .println(message)
+          println()
+          println(diffString(ciText, newText))
+          println()
+        } else {
+          val message =
+            "The test shard matrix in the CI file is out of date.\n" +
+              "\tfile://${yamlFile.get()}\n\n" +
+              "Run ./gradlew ${updateTaskName.get()} to automatically update."
 
-        println()
-        println(diffString(ciText, newText))
-        println()
-      } else {
-        val message = "The test shard matrix in the CI file is out of date.\n" +
-          "\tfile://${yamlFile.get()}\n\n" +
-          "Run ./gradlew ${updateTaskName.get()} to automatically update."
+          throw GradleException(message)
+        }
+      }
+    }
+
+    private fun replaceYamlSections(ciText: String): String {
+
+      if (!ciText.contains(matrixSectionRegex)) {
+        val message =
+          "Couldn't find any `$startTag`/`$endTag` sections in the CI file:" +
+            "\tfile://${yamlFile.get()}\n\n" +
+            "\tSurround the matrix section with the comments '$startTag' and `$endTag':\n\n" +
+            "\t    strategy:\n" +
+            "\t      ### $startTag\n" +
+            "\t      matrix:\n" +
+            "\t        [ ... ]\n" +
+            "\t      ### $endTag\n"
 
         throw GradleException(message)
       }
-    }
-  }
 
-  private fun replaceYamlSections(ciText: String): String {
+      return ciText.replace(matrixSectionRegex) { match ->
 
-    if (!ciText.contains(matrixSectionRegex)) {
-      val message =
-        "Couldn't find any `$startTag`/`$endTag` sections in the CI file:" +
-          "\tfile://${yamlFile.get()}\n\n" +
-          "\tSurround the matrix section with the comments '$startTag' and `$endTag':\n\n" +
-          "\t    strategy:\n" +
-          "\t      ### $startTag\n" +
-          "\t      matrix:\n" +
-          "\t        [ ... ]\n" +
-          "\t      ### $endTag\n"
+        val (indent, startTag, closingLine) = match.destructured
 
-      throw GradleException(message)
-    }
+        val newContent = createYaml(indent, numShards.get())
 
-    return ciText.replace(matrixSectionRegex) { match ->
-
-      val (indent, startTag, closingLine) = match.destructured
-
-      val newContent = createYaml(indent, numShards.get())
-
-      "$indent$startTag$newContent$closingLine"
-    }
-  }
-
-  private fun requireCiFile(): File {
-    val ciFile = yamlFile.get().asFile
-
-    require(ciFile.exists()) {
-      "Could not resolve file: file://$ciFile"
-    }
-
-    return ciFile
-  }
-
-  private fun createYaml(
-    indent: String,
-    numShards: Int
-  ): String {
-
-    val shardList = buildString {
-      append("[ ")
-      repeat(numShards) {
-        val i = it + 1
-        append("$i")
-        if (i < numShards) append(", ")
+        "$indent$startTag$newContent$closingLine"
       }
-      append(" ]")
     }
 
-    return "${indent}shardNum : $shardList\n"
-  }
+    private fun requireCiFile(): File {
+      val ciFile = yamlFile.get().asFile
 
-  companion object {
-    /**
-     * Registers tasks to check and update the test shard matrix configuration in `kotlin.yml`.
-     *
-     * @param shardCount The number of test shards.
-     * @param startTagName The start tag to identify the matrix section in `kotlin.yml`.
-     * @param endTagName The end tag to identify the matrix section in `kotlin.yml`.
-     * @param taskNamePart The part of the sharded task name which will be prepended to
-     *   the matrix update task names.
-     * @param yamlFile presumably `kotlin.yml`.
-     */
-    fun Project.registerYamlShardsTasks(
-      shardCount: Int,
-      startTagName: String,
-      endTagName: String,
-      taskNamePart: String,
-      yamlFile: File
-    ) {
-
-      require(yamlFile.exists()) {
-        "Could not resolve '$yamlFile'."
+      require(ciFile.exists()) {
+        "Could not resolve file: file://$ciFile"
       }
 
-      val updateName = "${taskNamePart}ShardMatrixYamlUpdate"
-      val updateTask = tasks.register(
-        updateName,
-        ShardMatrixYamlTask::class.java
-      ) { task ->
-        task.yamlFile.set(yamlFile)
-        task.numShards.set(shardCount)
-        task.startTagProperty.set(startTagName)
-        task.endTagProperty.set(endTagName)
-        task.autoCorrect.set(true)
-        task.updateTaskName.set(updateName)
-      }
+      return ciFile
+    }
 
-      val checkTask = tasks.register(
-        "${taskNamePart}ShardMatrixYamlCheck",
-        ShardMatrixYamlTask::class.java
-      ) { task ->
-        task.yamlFile.set(yamlFile)
-        task.numShards.set(shardCount)
-        task.startTagProperty.set(startTagName)
-        task.endTagProperty.set(endTagName)
-        task.autoCorrect.set(false)
-        task.updateTaskName.set(updateName)
-        task.mustRunAfter(updateTask)
-      }
+    private fun createYaml(
+      indent: String,
+      numShards: Int
+    ): String {
 
-      // Automatically run this check task when running the `check` lifecycle task
-      tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).dependsOn(checkTask)
+      val shardList =
+        buildString {
+          append("[ ")
+          repeat(numShards) {
+            val i = it + 1
+            append("$i")
+            if (i < numShards) append(", ")
+          }
+          append(" ]")
+        }
+
+      return "${indent}shardNum: $shardList\n"
+    }
+
+    companion object {
+      /**
+       * Registers tasks to check and update the test shard matrix configuration in `kotlin.yml`.
+       *
+       * @param shardCount The number of test shards.
+       * @param startTagName The start tag to identify the matrix section in `kotlin.yml`.
+       * @param endTagName The end tag to identify the matrix section in `kotlin.yml`.
+       * @param taskNamePart The part of the sharded task name which will be prepended to
+       *   the matrix update task names.
+       * @param yamlFile presumably `kotlin.yml`.
+       */
+      fun Project.registerYamlShardsTasks(
+        shardCount: Int,
+        startTagName: String,
+        endTagName: String,
+        taskNamePart: String,
+        yamlFile: File
+      ) {
+
+        require(yamlFile.exists()) {
+          "Could not resolve '$yamlFile'."
+        }
+
+        val updateName = "${taskNamePart}ShardMatrixYamlUpdate"
+        val updateTask =
+          tasks.register(
+            updateName,
+            ShardMatrixYamlTask::class.java
+          ) { task ->
+            task.yamlFile.set(yamlFile)
+            task.numShards.set(shardCount)
+            task.startTagProperty.set(startTagName)
+            task.endTagProperty.set(endTagName)
+            task.autoCorrect.set(true)
+            task.updateTaskName.set(updateName)
+          }
+
+        val checkTask =
+          tasks.register(
+            "${taskNamePart}ShardMatrixYamlCheck",
+            ShardMatrixYamlTask::class.java
+          ) { task ->
+            task.yamlFile.set(yamlFile)
+            task.numShards.set(shardCount)
+            task.startTagProperty.set(startTagName)
+            task.endTagProperty.set(endTagName)
+            task.autoCorrect.set(false)
+            task.updateTaskName.set(updateName)
+            task.mustRunAfter(updateTask)
+          }
+
+        // Automatically run this check task when running the `check` lifecycle task
+        tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).dependsOn(checkTask)
+      }
     }
   }
-}
