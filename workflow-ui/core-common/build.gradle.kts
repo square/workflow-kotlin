3a482111cca1426d68faca779205fdb5b9ef4fe0
Min Hiew <mhiew@squareup.com>
2025-02-12 14:56:11 -0500

    [DNM] Enable Stability Flags on Screen and ComposeScreen

--- workflow-ui/core-common/build.gradle.kts
+++ workflow-ui/core-common/build.gradle.kts
@@ -4,9 +4,13 @@ plugins {
 }
 
 dependencies {
+  val composeBom = platform(libs.androidx.compose.bom)
+  implementation(composeBom)
+
   api(libs.kotlin.jdk6)
   api(libs.kotlinx.coroutines.core)
   api(libs.squareup.okio)
+  api(libs.androidx.compose.runtime)
 
   testImplementation(libs.junit)
   testImplementation(libs.kotlin.test.core)
--- workflow-ui/core-common/src/main/java/com/squareup/workflow1/ui/Screen.kt
+++ workflow-ui/core-common/src/main/java/com/squareup/workflow1/ui/Screen.kt
@@ -1,7 +1,11 @@
 package com.squareup.workflow1.ui
 
+import androidx.compose.runtime.Stable
+
 /**
  * Marker interface implemented by renderings that map to a UI system's 2d view class.
  */
+
+@Stable
 @WorkflowUiExperimentalApi
 public interface Screen
