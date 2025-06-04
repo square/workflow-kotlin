56de1f6c8a11520e86b2b7f84e32976ee02c38fb
Wenli Cai <wenli@squareup.com>
2025-06-04 14:51:40 -0400

    WIP provide temporary solution for drawing arrows between each workflow node
    - arrows start with a weird offset in the beginning relative to where the nodes are

---
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/Arrow.kt
@@ -0,0 +1,88 @@
+package com.squareup.workflow1.traceviewer
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.dp
+import kotlin.math.atan2
+
+/**
+ * Since we eventually would want the ability to click into each arrow between nodes and see what
+ * state/props are being passed, having a custom composable for the arrow where an onArrowClick
+ * could be set will be helpful.
+ */
+@Composable
+public fun Arrow(
+  start: Offset,
+  end: Offset,
+) {
+  Box(
+    modifier = Modifier
+      .size(20.dp)
+      .clickable { print("clicked") }
+  ) {
+    Text("top left corner")
+    Canvas (modifier = Modifier.fillMaxSize()){
+      drawArrow(
+        start = start,
+        end = end,
+        color = Color.Black,
+        strokeWidth = 2f
+      )
+    }
+  }
+}
+
+// iterative
+// @Composable
+// public fun drawAllArrows(
+//   arrowLocations: List<Pair<NodePosition, NodePosition>>,
+// ) {
+//   Canvas(modifier = Modifier.fillMaxSize()) {
+//     arrowLocations.forEach { (start, end) ->
+//       drawArrow(
+//         start = start.position,
+//         end = end.position,
+//         color = Color.Black,
+//         strokeWidth = 2f
+//       )
+//     }
+//   }
+// }
+
+
+private fun DrawScope.drawArrow(
+  start: Offset,
+  end: Offset,
+  color: Color,
+  strokeWidth: Float
+) {
+  drawLine(
+    color = color,
+    start = start,
+    end = end,
+    strokeWidth = strokeWidth
+  )
+
+  val arrowHeadSize = 20f
+  val angle = atan2((end.y - start.y).toDouble(), (end.x - start.x).toDouble()).toFloat()
+  val arrowPoint1 = Offset(
+    x = end.x - arrowHeadSize * Math.cos(angle + Math.PI / 6).toFloat(),
+    y = end.y - arrowHeadSize * Math.sin(angle + Math.PI / 6).toFloat()
+  )
+  val arrowPoint2 = Offset(
+    x = end.x - arrowHeadSize * Math.cos(angle - Math.PI / 6).toFloat(),
+    y = end.y - arrowHeadSize * Math.sin(angle - Math.PI / 6).toFloat()
+  )
+
+  drawLine(color, end, arrowPoint1, strokeWidth)
+  drawLine(color, end, arrowPoint2, strokeWidth)
+}
