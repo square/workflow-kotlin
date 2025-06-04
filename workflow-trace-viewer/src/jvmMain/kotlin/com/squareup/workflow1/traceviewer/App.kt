8e11a482bca7c6a2a287739e5550cdae2581adbb
Wenli Cai <wenli@squareup.com>
2025-06-04 11:46:00 -0400

    Create json parsing using moshi

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
@@ -5,5 +5,12 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun App() {
-  Text("Hello world!")
+  val jsonString = object {}.javaClass.getResource("/workflow-simple.json")?.readText()
+  val root = jsonString?.let { FetchRoot(it) }
+  println(root)
+  if (root != null) {
+    DrawWorkflowTree(root)
+  } else {
+    Text("Empty data or failed to parse data") // TODO: proper handling of error
+  }
 }
