142e9ce78b4903375d94a9333da62af6451bd713
Wenli Cai <wenli@squareup.com>
2025-06-05 14:19:03 -0400

    Removed use of arrows in project, cleaned up code
    - Figuring out how to lay out arrows was unnecessarily difficult, and ended up being too distracting,
    so utilizing the column space each node and its children are in was a much cleaner way to represent a
    subtree relationship
    - Figure will closely resemble a treemap

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
@@ -2,15 +2,41 @@ package com.squareup.workflow1.traceviewer
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.squareup.workflow1.traceviewer.FileReader.fetchFile
+import io.github.vinceglb.filekit.PlatformFile
 
 @Composable
 fun App() {
-  val jsonString = object {}.javaClass.getResource("/workflow-simple.json")?.readText()
+  val jsonString = fetchFile()
   val root = jsonString?.let { FetchRoot(it) }
-  println(root)
+
+  var selectedFile = remember { mutableStateOf<PlatformFile?>(null)}
+
+  // UploadFile { selectedFile.value = it }
+
   if (root != null) {
     DrawWorkflowTree(root)
   } else {
     Text("Empty data or failed to parse data") // TODO: proper handling of error
   }
 }
+
+
+object FileReader {
+  fun fetchFile() : String?{
+    return javaClass.getResource("/workflow-simple.json")?.readText()
+  }
+}
+
+//
+// @Composable
+// private fun UploadFile(onFileSelected: (PlatformFile?) -> Unit) {
+//   val launcher = rememberFilePickerLauncher(
+//     type = FileKitType.Custom(listOf("json","txt")),
+//     title = "Select Workflow Trace File"
+//   ) {
+//     onFileSelected(it)
+//   }
+// }
