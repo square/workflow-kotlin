46f2f206a177c7351537af8a0339ff6acf91a319
Wenli Cai <wenli@squareup.com>
2025-06-05 14:19:03 -0400

    Removed use of arrows in project, cleaned up code
    - Figuring out how to lay out arrows was unnecessarily difficult, and ended up being too distracting,
    so utilizing the column space each node and its children are in was a much cleaner way to represent a
    subtree relationship
    - Figure will closely resemble a treemap

--- workflow-trace-viewer/build.gradle.kts
+++ workflow-trace-viewer/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
         implementation(libs.kotlinx.coroutines.swing)
         implementation(compose.materialIconsExtended)
         implementation(libs.moshi.kotlin)
+        implementation("io.github.vinceglb:filekit-dialogs-compose:0.10.0-beta03")
       }
     }
   }
