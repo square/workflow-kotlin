6b30019019ed6a5e8e1abc45ad1b08b1bc4c1636
Wenli Cai <wenli@squareup.com>
2025-06-05 16:11:24 -0400

    create upload button for downloaded json/txt files

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/Main.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/Main.kt
@@ -3,8 +3,7 @@ package com.squareup.workflow1.traceviewer
 import androidx.compose.ui.window.singleWindowApplication
 
 fun main() {
-  // FileKit.init(appId = "com.squareup.workflow1.traceviewer")
   singleWindowApplication(title = "Workflow Trace Viewer") {
-    SandboxBackground { App() }
+    App()
   }
 }
