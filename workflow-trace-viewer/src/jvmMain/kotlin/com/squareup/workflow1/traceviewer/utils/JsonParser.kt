2e84b41477020f28d343b4a24cc2ed82ed88c088
Wenli Cai <wenli@squareup.com>
2025-06-10 11:30:30 -0400

    Edit composables to align with their responsibilities
    The order of the main content being displayed was from parsing -> rendering, which would lead to complexity when adding in tabbing functionality.
    Having the order be Renderer -> parsing -> rendering was more clear.
    This commit also introduces the idea of a "trace", where we have input a json array instead of a singular json

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/utils/JsonParser.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/utils/JsonParser.kt
@@ -1,16 +1,12 @@
 package com.squareup.workflow1.traceviewer.utils
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.squareup.workflow1.traceviewer.model.WorkflowNode
-import com.squareup.workflow1.traceviewer.ui.DrawWorkflowTree
 import io.github.vinceglb.filekit.PlatformFile
 import io.github.vinceglb.filekit.readString
 import java.io.IOException
@@ -18,23 +14,12 @@ import java.io.IOException
 /**
  * Parses the data from the given file and initiates the workflow tree
  */
-@Composable
-public fun LoadWorkflowContent(
+public suspend fun fetchTrace(
   file: PlatformFile?,
-  onNodeSelect: (WorkflowNode) -> Unit,
   modifier: Modifier = Modifier
-) {
-  val jsonString = remember { mutableStateOf<String?>(null) }
-  LaunchedEffect(file) {
-    jsonString.value = file?.readString()
-  }
-  val root = jsonString.value?.let { fetchRoot(it) }
-
-  if (root != null) {
-    DrawWorkflowTree(root, onNodeSelect)
-  } else {
-    Text("Empty data or failed to parse data") // TODO: proper handling of error
-  }
+): List<WorkflowNode> {
+  val jsonString = file?.readString()
+  return jsonString?.let { parseTrace(it) } ?: emptyList()
 }
 
 /**
@@ -43,15 +28,17 @@ public fun LoadWorkflowContent(
  * All the caught exceptions should be handled by the caller, and appropriate UI feedback should be
  * provided to user
  */
-public fun fetchRoot(
+public fun parseTrace(
   json: String
-): WorkflowNode? {
+): List<WorkflowNode> {
   return try {
     val moshi = Moshi.Builder()
       .add(KotlinJsonAdapterFactory())
       .build()
-    val workflowAdapter = moshi.adapter(WorkflowNode::class.java)
-    val root = workflowAdapter.fromJson(json)
+
+    val workflowList = Types.newParameterizedType(List::class.java, WorkflowNode::class.java)
+    val workflowAdapter: JsonAdapter<List<WorkflowNode>> = moshi.adapter(workflowList)
+    val root = workflowAdapter.fromJson(json) ?: emptyList()
     root
   } catch (e: JsonDataException) {
     throw JsonDataException("Failed to parse JSON: ${e.message}", e)
