54d7b92b1cd0760e3ea1f1caae725e8afd7c221e
Wenli Cai <wenli@squareup.com>
2025-06-10 11:30:30 -0400

    Edit composables to align with their responsibilities
    The order of the main content being displayed was from parsing -> rendering, which would lead to complexity when adding in tabbing functionality.
    Having the order be Renderer -> parsing -> rendering was more clear.
    This commit also introduces the idea of a "trace", where we have input a json array instead of a singular json

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/App.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.squareup.workflow1.traceviewer.model.WorkflowNode
 import com.squareup.workflow1.traceviewer.ui.InfoPanel
-import com.squareup.workflow1.traceviewer.utils.LoadWorkflowContent
+import com.squareup.workflow1.traceviewer.ui.RenderDiagram
 import com.squareup.workflow1.traceviewer.utils.SandboxBackground
 import com.squareup.workflow1.traceviewer.utils.UploadFile
 import io.github.vinceglb.filekit.PlatformFile
@@ -24,6 +26,8 @@ public fun App(
 ) {
   var selectedFile by remember { mutableStateOf<PlatformFile?>(null) }
   var selectedNode by remember { mutableStateOf<WorkflowNode?>(null) }
+  var snapshotIndex by remember { mutableIntStateOf(0) }
+
   // Used when user selects a new file in [UploadFile]
   val resetSelectedNode = { selectedNode = null }
 
@@ -31,16 +35,19 @@ public fun App(
     // Main content
     if (selectedFile != null) {
       SandboxBackground {
-        LoadWorkflowContent(selectedFile) {
-          selectedNode = it
-        }
+        RenderDiagram(
+          file = selectedFile,
+          traceInd = snapshotIndex,
+          onNodeSelect = { selectedNode = it }
+        )
       }
     }
 
+    // Top trace selector row
+    // TraceSelectRow { snapshotIndex.value = it }
+
     // Left side information panel
-    InfoPanel(
-      selectedNode
-    )
+    InfoPanel(selectedNode)
 
     // Bottom right upload button
     UploadFile(resetSelectedNode, { selectedFile = it })
