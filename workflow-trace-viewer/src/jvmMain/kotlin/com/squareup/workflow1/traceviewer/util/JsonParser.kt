4255a8f1c857c8ce5cc9924aad3ee6865c696191
Wenli Cai <wenli@squareup.com>
2025-06-13 12:07:33 -0400

    More fix for PR comments
    1. Clarified component positions with modifiers
    2. Removed unnecessary comments
    3. Simplified JsonParser

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/util/JsonParser.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/util/JsonParser.kt
@@ -1,6 +1,5 @@
 package com.squareup.workflow1.traceviewer.util
 
-import androidx.compose.ui.Modifier
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
@@ -12,31 +11,25 @@ import io.github.vinceglb.filekit.readString
 import java.io.IOException
 
 /**
- * Parses the data from the given file and initiates the workflow tree
- */
-public suspend fun fetchTrace(
-  file: PlatformFile?,
-  modifier: Modifier = Modifier
-): List<Node> {
-  val jsonString = file?.readString()
-  return jsonString?.let { parseTrace(it) } ?: emptyList()
-}
-
-/**
- * Parses a JSON string into [Node] with Moshi adapters. Moshi automatically throws JsonDataException
+ * Parses a given file's JSON String into [Node] with Moshi adapters. Moshi automatically throws JsonDataException
  * and IOException
  * @throws JsonDataException malformed JSON data or an error reading.
  * @throws IOException JSON is correct, but mismatch between class and JSON structure.
  */
-public fun parseTrace(
-  json: String
+public suspend fun parseTrace(
+  file: PlatformFile,
 ): List<Node> {
+  val jsonString = file.readString()
+
   val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
   val workflowList = Types.newParameterizedType(List::class.java, Node::class.java)
   val workflowAdapter: JsonAdapter<List<Node>> = moshi.adapter(workflowList)
-  val root = workflowAdapter.fromJson(json) ?: emptyList()
-  return root
+  val trace = workflowAdapter.fromJson(jsonString)
+  if (trace == null) {
+    throw JsonDataException("Could not parse JSON")
+  }
+  return trace
 }
