a1584145ed471d06a0b1d2f0916048882e513baa
Zach Klippenstein <git@zachklipp.com>
2025-06-23 17:30:15 -0700

    Introduce WorkStealingDispatcher.

---
+++ workflow-runtime/src/jsMain/kotlin/com/squareup/workflow1/internal/Synchronization.js.kt
@@ -0,0 +1,7 @@
+package com.squareup.workflow1.internal
+
+// JS doesn't have threading, so doesn't need any actual synchronization.
+
+internal actual typealias Lock = Any
+
+internal actual inline fun <R> Lock.withLock(block: () -> R): R = block()
