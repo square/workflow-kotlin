57dbae22404560a8c0eb561ae4a5c3e8539009b6
Wenli Cai <wenli@squareup.com>
2025-07-14 18:30:52 -0400

    Change coroutine scope

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/ui/FrameSelectTab.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/ui/FrameSelectTab.kt
@@ -3,7 +3,6 @@ package com.squareup.workflow1.traceviewer.ui
 import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -11,7 +10,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -19,6 +17,7 @@ import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.squareup.workflow1.traceviewer.model.Node
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 /**
@@ -32,7 +31,6 @@ internal fun FrameSelectTab(
   modifier: Modifier = Modifier
 ) {
   val lazyListState = rememberLazyListState()
-  val coroutineScope = rememberCoroutineScope()
 
   Surface(
     modifier = modifier,
@@ -43,13 +41,15 @@ internal fun FrameSelectTab(
       modifier = Modifier
         .padding(8.dp)
         .pointerInput(Unit) {
-          awaitEachGesture {
-            val event = awaitPointerEvent()
-            if (event.type == PointerEventType.Scroll) {
-              val scrollDeltaY = event.changes.first().scrollDelta.y
-              coroutineScope.launch {
-                lazyListState.scroll(MutatePriority.Default) {
-                  scrollBy(scrollDeltaY * 10f)
+          coroutineScope {
+            awaitEachGesture {
+              val event = awaitPointerEvent()
+              if (event.type == PointerEventType.Scroll) {
+                val scrollDeltaY = event.changes.first().scrollDelta.y
+                launch {
+                  lazyListState.scroll(MutatePriority.Default) {
+                    scrollBy(scrollDeltaY * 10f)
+                  }
                 }
               }
             }
