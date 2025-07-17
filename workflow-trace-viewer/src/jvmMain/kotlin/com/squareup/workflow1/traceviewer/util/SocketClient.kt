c63c43a271a169bb1f976a3bab15029f135ccca7
Wenli Cai <wenli@squareup.com>
2025-07-17 14:55:59 -0400

    Extract socket setup and reading logic.

--- workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/util/SocketClient.kt
+++ workflow-trace-viewer/src/jvmMain/kotlin/com/squareup/workflow1/traceviewer/util/SocketClient.kt
@@ -1,51 +1,62 @@
 package com.squareup.workflow1.traceviewer.util
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.channels.Channel
 import java.net.Socket
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import com.squareup.workflow1.traceviewer.model.Node
 
-class SocketClient {
-  private var socket: Socket
-  private val scope = CoroutineScope(Dispatchers.IO)
+/**
+ * This is a client that connects to the `ActionLogger` Unix Domain Socket and listens for any new
+ * render passes. Since this app is on JVM and the server is on Android, we use ADB to forward the
+ * port onto the socket.
+ */
+internal class SocketClient {
+  private lateinit var socket: Socket
+  private var initialized = false
+  val renderPassChannel: Channel<String> = Channel(Channel.UNLIMITED)
 
-  init{
-    val moshi = Moshi.Builder()
-      .add(KotlinJsonAdapterFactory())
-      .build()
-    val workflowList = Types.newParameterizedType(List::class.java, Node::class.java)
-    val adapter: JsonAdapter<List<Node>> = moshi.adapter(workflowList)
-
+  /**
+   * We use any available ports on the host machine to connect to the emulator.
+   *
+   * `workflow-trace` is the name of the unix socket created, and since Android uses
+   * `LocalServerSocket` -- which creates a unix socket on the linux abstract namespace -- we use
+   * `localabstract:` to connect to it.
+   */
+  fun start() {
+    initialized = true
     val process = ProcessBuilder(
       "adb", "forward", "tcp:0", "localabstract:workflow-trace"
     ).start()
 
+    // The adb forward command will output the port number it picks to connect.
     val port = run {
       process.waitFor()
       process.inputStream.bufferedReader().readText()
         .trim().toInt()
     }
-    println(port)
+    // println(port)
     socket = Socket("localhost", port)
-    println(socket)
+    // println("Connected to workflow trace server on port: $port")
+  }
 
-    println("Connected to workflow trace server on port: $port")
-    var str = ""
-    scope.launch {
-      val reader = socket.getInputStream().bufferedReader()
-      while (true) {
-        // val reader = socket.getInputStream().bufferedReader()
-        val input = reader.readLine()
-        str = input
-        println("Received: $input")
-        val renderpass = adapter.fromJson(str)
-        println(renderpass)
-      }
+  fun close() {
+    if (!initialized) {
+      return
+    }
+    socket.close()
+    initialized = false
+  }
+
+  /**
+   * This will always be called within an asynchronous call, so we do not need to block/launch a
+   * new coroutine here.
+   *
+   * To better separate the responsibility of reading from the socket, we use a channel for the caller
+   * to handle parsing and amalgamating the render passes.
+   */
+  fun beginListen() {
+    val reader = socket.getInputStream().bufferedReader()
+    while (true) {
+      val input = reader.readLine()
+      renderPassChannel.trySend(input)
     }
   }
 }
