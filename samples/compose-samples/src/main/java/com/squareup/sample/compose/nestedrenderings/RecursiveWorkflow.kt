f167d866aa28a76307bfdd426833fd5002c53320
Zach Klippenstein <git@zachklipp.com>
2026-01-14 15:56:13 -0800

    Make nested renderings flash when clicked, to help test output propagation.

--- samples/compose-samples/src/main/java/com/squareup/sample/compose/nestedrenderings/RecursiveWorkflow.kt
+++ samples/compose-samples/src/main/java/com/squareup/sample/compose/nestedrenderings/RecursiveWorkflow.kt
@@ -1,16 +1,23 @@
 package com.squareup.sample.compose.nestedrenderings
 
+import androidx.compose.ui.util.fastForEach
 import com.squareup.sample.compose.databinding.LegacyViewBinding
 import com.squareup.sample.compose.nestedrenderings.RecursiveWorkflow.LegacyRendering
 import com.squareup.sample.compose.nestedrenderings.RecursiveWorkflow.Rendering
 import com.squareup.sample.compose.nestedrenderings.RecursiveWorkflow.State
+import com.squareup.workflow1.SessionWorkflow
 import com.squareup.workflow1.Snapshot
-import com.squareup.workflow1.StatefulWorkflow
+import com.squareup.workflow1.WorkflowExperimentalApi
 import com.squareup.workflow1.action
 import com.squareup.workflow1.renderChild
 import com.squareup.workflow1.ui.AndroidScreen
 import com.squareup.workflow1.ui.Screen
 import com.squareup.workflow1.ui.ScreenViewFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.ZERO
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * A simple workflow that produces [Rendering]s of zero or more children.
@@ -20,9 +27,16 @@ import com.squareup.workflow1.ui.ScreenViewFactory
  * to force it to go through the legacy view layer. This way this sample both demonstrates pass-
  * through Composable renderings as well as adapting in both directions.
  */
-object RecursiveWorkflow : StatefulWorkflow<Unit, State, Nothing, Screen>() {
+@OptIn(WorkflowExperimentalApi::class)
+object RecursiveWorkflow : SessionWorkflow<Unit, State, Unit, Screen>() {
 
-  data class State(val children: Int = 0)
+  data class State(
+    val children: Int = 0,
+    val flashTrigger: Int = 0,
+    val nextFlashId: Int = 0,
+    val pendingFlashes: List<Int> = emptyList(),
+    val flashScope: CoroutineScope,
+  )
 
   /**
    * A rendering from a [RecursiveWorkflow].
@@ -33,6 +47,9 @@ object RecursiveWorkflow : StatefulWorkflow<Unit, State, Nothing, Screen>() {
    */
   data class Rendering(
     val children: List<Screen>,
+    val flashTrigger: Int = 0,
+    val flashTime: Duration = ZERO,
+    val onSelfClicked: () -> Unit = {},
     val onAddChildClicked: () -> Unit,
     val onResetClicked: () -> Unit
   ) : Screen
@@ -51,21 +68,38 @@ object RecursiveWorkflow : StatefulWorkflow<Unit, State, Nothing, Screen>() {
 
   override fun initialState(
     props: Unit,
-    snapshot: Snapshot?
-  ): State = State()
+    snapshot: Snapshot?,
+    workflowScope: CoroutineScope
+  ): State = State(flashScope = workflowScope)
 
   override fun render(
     renderProps: Unit,
     renderState: State,
-    context: RenderContext<Unit, State, Nothing>
+    context: RenderContext<Unit, State, Unit>
   ): Rendering {
+    renderState.pendingFlashes.fastForEach {
+      context.runningSideEffect("flash[$it]") {
+        delay(0.1.seconds)
+        context.actionSink.send(flashImmediately(id = it))
+      }
+    }
+
     return Rendering(
       children = List(renderState.children) { i ->
-        val child = context.renderChild(RecursiveWorkflow, key = i.toString())
+        val child = context.renderChild(
+          RecursiveWorkflow,
+          key = i.toString(),
+          // When a child is clicked, cascade the flash up.
+          handler = { flashAfterDelay() }
+        )
         if (i % 2 == 0) child else LegacyRendering(child)
       },
+      flashTrigger = renderState.flashTrigger,
+      flashTime = 0.5.seconds,
+      // Trigger a cascade of flashes when clicked.
+      onSelfClicked = { context.actionSink.send(flashImmediately()) },
       onAddChildClicked = { context.actionSink.send(addChild()) },
-      onResetClicked = { context.actionSink.send(reset()) }
+      onResetClicked = { context.actionSink.send(resetChildren()) }
     )
   }
 
@@ -75,7 +109,27 @@ object RecursiveWorkflow : StatefulWorkflow<Unit, State, Nothing, Screen>() {
     state = state.copy(children = state.children + 1)
   }
 
-  private fun reset() = action("reset") {
-    state = State()
+  private fun resetChildren() = action("resetChildren") {
+    state = state.copy(
+      children = 0,
+      pendingFlashes = emptyList(),
+    )
+  }
+
+  private fun flashImmediately(id: Int? = null) = action("triggerImmediateFlash[id=$id]") {
+    val newPendingFlashes = if (id == null) state.pendingFlashes else state.pendingFlashes - id
+    state = state.copy(
+      flashTrigger = state.flashTrigger + 1,
+      pendingFlashes = newPendingFlashes,
+    )
+    setOutput(Unit)
+  }
+
+  private fun flashAfterDelay() = action("triggerDelayedFlash") {
+    val newFlash = state.nextFlashId
+    state = state.copy(
+      nextFlashId = state.nextFlashId + 1,
+      pendingFlashes = state.pendingFlashes + newFlash
+    )
   }
 }
